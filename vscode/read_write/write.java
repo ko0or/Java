65dc56039356b8460bee3ed9d1b5e8621b1c132a
minwoo <105141296+ko0or@users.noreply.github.com>
2023-02-06 22:12:03 +0900

    Update write.java

--- vscode/read_write/write.java
+++ vscode/read_write/write.java
@@ -1,5 +1,4 @@
 package read_write;
-
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -10,11 +9,8 @@ public class write {
     public static void main(String[] args) throws IOException {
 
             String path = "./writeFile.txt";
-            Files.write(Paths.get(path), "안녕하세요\n동해물과\n백두산이\nabc".getBytes());
-            
-            
-           
- 
+            Files.write( Paths.get(path), "안녕하세요\n동해물과\n백두산이\nabc".getBytes() );
+            // Files.write( Paths.get(path), "안녕하세요\n동해물과\n백두산이\nabc".getBytes(), StandardOpenOption.APPEND );
 
     }
 }
