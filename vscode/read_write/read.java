b0edcb47d7c6cb0dc3dee9af6e222401c39c1924
minwoo <105141296+ko0or@users.noreply.github.com>
2023-02-06 22:08:31 +0900

    Add files via upload

--- vscode/read_write/read.java
+++ vscode/read_write/read.java
@@ -1,29 +1,26 @@
 package read_write;
-import java.io.IOException;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
- 
+
+
+
+
 public class read {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("./read_write/writeFile.txt");
+       
+        String Path = "./writeFile.txt";
+        byte[] bytes = Files.readAllBytes(Paths.get(Path)); 
 
-        // 파일내용 전체 읽어오기
-        String str = Files.readString(path, StandardCharsets.UTF_8);
-        System.out.println(str);
-        System.out.println("------------------>");
+        String str = new String(bytes);
+        //String[] str = new String(bytes).split("\n");
+
+        System.out.println( str );
         
 
-        // 0번째 글자 확인하기
-        System.out.println( str.charAt(0) );
 
-        // 불러온 텍스트에 백두산이 있는지 확인 (boolean)
-        System.out.println( str.contains("백두산") );
-
-        // 불러온 텍스트중에서 ` 물 ` 이라는 글자가 몇번쨰 위치하는지
-        System.out.println( str.indexOf("물") );
+        
  
     }
 }
--- vscode/read_write/write.java
+++ vscode/read_write/write.java
@@ -1,24 +1,20 @@
 package read_write;
+
 import java.io.IOException;
+
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
  
 public class write {
-    public static void main(String[] args) {
-        try {
- 
-            // 1. Path 객체 생성
-            Path path = Paths.get("./read_write/writeFile.txt");
- 
-            // 2. 파일에 쓰기
-            Files.write(path, "안녕하세요".getBytes());
-            
+    public static void main(String[] args) throws IOException {
 
+            String path = "./riteFile.txt";
+            Files.write(Paths.get(path), "안녕하세요\n동해물과\n백두산이\nabc".getBytes());
             
+            
+           
  
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
