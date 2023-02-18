50fa08b65dad12e4185ef7d8b039d175c324ed6f
minwoo <105141296+ko0or@users.noreply.github.com>
2023-02-19 03:25:40 +0900

    Update Base.java

--- vscode/MethodChaining/Base.java
+++ vscode/MethodChaining/Base.java
@@ -1,13 +1,20 @@
 class Base {
     // 클래스 필드 (맴버변수)
     private String name;
+    private String job;
     private int age;
 
-    // 생성자    
-    Base() {}
-    Base(String name, int age) {
+    
+    // 생성자 오버로딩
+    Base() {
+        this.name = "무명";
+        this.job = "떠돌이";
+        this.age = 1;
+    }
+    Base(String name, int age, String job) {
         this.name = name;
         this.age = age;
+        this.job = job;
     }
 
     // 세터 메소드
@@ -21,11 +28,20 @@ class Base {
         this.age = age;
         return this;
     }
+
+    //세터 메소드
+    public String setJob(String job) {
+        this.job = job;
+        return this.job;
+    }
     
     // 오버라이딩 toString()
     @Override
     public String toString() {
-        return getClass() + " [name=" + name + ", age=" + age + "]";
+        return " [name=" + name 
+        + ", age=" + age 
+        + ", job=" + job 
+        + "]";
     }
     
 }
