643715405fcbee6924b6daf4adb2ef8bbeea2389
minwoo <105141296+ko0or@users.noreply.github.com>
2023-02-19 04:32:05 +0900

    Update FILE01.java

--- vscode/MethodChaining/FILE01.java
+++ vscode/MethodChaining/FILE01.java
@@ -2,15 +2,13 @@ public class FILE01 {
     public static void main(String[] args) {
         /* --- Base.java 파일을 활용한 예제 --- */
         
-        // chain_01, 메소드체이닝 사용(세터 2개)
+        // chain_01, 메소드체이닝 사용(세터 3개)
         Base chain_01 = new Base();
         chain_01
             .setName("김모씨")
             .setAge(20)
             .setJob("자영업");
             
-            
-        
 
         // chain_02, 생성자 사용
         Base chain_02 = new Base("이모씨", 22, "직장인");
